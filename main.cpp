9cdc3ddacaf273ef6b5fa227d92c56cf2bccddf5
Emera <emeraldedge_20230823@hotmail.com>
2024-03-26 05:43:47 +0900

    とりあえず点を表示

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <GameLib/Framework.h>
+
+
+namespace GameLib {
+
+	void Framework::update() {
+		unsigned* vram = videoMemory();
+		static unsigned i;
+		vram[i] += i * 100;
+		i += 9973; //1ñúà»â∫ç≈ëÂÇÃóvëf
+		i %= (width() * height());
+	}
+}
