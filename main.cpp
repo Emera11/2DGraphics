75595839584db03d0005540244c3472bb1cd6a6c
Emera <emeraldedge_20230823@hotmail.com>
2024-04-02 14:01:30 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,13 +1,65 @@
-#include <GameLib/Framework.h>
+#include <iostream>
+using namespace std;
+#include<Windows.h>
 
+int main(void){
 
-namespace GameLib {
+	//‰€Ž™‚Ìl”
+	int N;
+	cin >> N;
 
-	void Framework::update() {
-		unsigned* vram = videoMemory();
-		static unsigned i;
-		vram[i] += i * 100;
-		i += 9973; //1–œˆÈ‰ºÅ‘å‚Ì—v‘f
-		i %= (width() * height());
+	//Ÿ•‰”
+	int M;
+	cin >> M;
+
+	int Win;
+	int Lose;
+	int old_Win = 0;
+
+	int* Row = new int[N];
+	for (int i = 0; i < N; ++i)
+	{
+		Row[i] = 0;
 	}
+
+	int k = 0;
+
+	for (int i = 0; i < M; ++i)
+	{
+		cin  >> Win;
+
+		cin  >> Lose;
+
+		if (old_Win == Lose)
+		{
+			Row[Win - 1] += Row[Lose - 1];
+		}
+	
+		Row[Win - 1] += 1;
+
+		old_Win = Win;
+	}
+
+
+	for (int i = 0; i < N; ++i)
+	{
+	
+		if (k < Row[i])
+		{
+			k = Row[i];
+		}
+		
+	}
+
+
+	for (int i = 0; i < N; ++i)
+	{
+		if (k == Row[i])
+		{
+			cout << i+1 << endl;
+		}
+	}
+
+	system("PAUSE");
+	return 0;
 }
