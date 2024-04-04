7404e415d3a9206987dbb57536a4e8335c7f5057
Emera <emeraldedge_20230823@hotmail.com>
2024-04-04 16:04:09 +0900

    s

--- main.cpp
+++ main.cpp
@@ -1,65 +1,47 @@
-#include <iostream>
-using namespace std;
-#include<Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
+int main(void)
+{
+    char buffer[10]{'\0'};
+    char Old_buffer[10]{'\0'};
 
-	//‰€Ž™‚Ìl”
-	int N;
-	cin >> N;
+    const int k = (sizeof(buffer) / sizeof(buffer[0]));
+    printf("•¶Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢...\n\n");
 
-	//Ÿ•‰”
-	int M;
-	cin >> M;
-
-	int Win;
-	int Lose;
-	int old_Win = 0;
-
-	int* Row = new int[N];
-	for (int i = 0; i < N; ++i)
-	{
-		Row[i] = 0;
-	}
-
-	int k = 0;
-
-	for (int i = 0; i < M; ++i)
-	{
-		cin  >> Win;
-
-		cin  >> Lose;
-
-		if (old_Win == Lose)
-		{
-			Row[Win - 1] += Row[Lose - 1];
-		}
-	
-		Row[Win - 1] += 1;
-
-		old_Win = Win;
-	}
+ 
+     fgets(buffer, sizeof(buffer), stdin)
 
 
-	for (int i = 0; i < N; ++i)
-	{
-	
-		if (k < Row[i])
-		{
-			k = Row[i];
-		}
-		
-	}
+    int Num = 0;
+
+    memcpy(Old_buffer, buffer, sizeof(buffer));
 
 
-	for (int i = 0; i < N; ++i)
-	{
-		if (k == Row[i])
-		{
-			cout << i+1 << endl;
-		}
-	}
+    
 
-	system("PAUSE");
-	return 0;
+    while (buffer[Num] != '\0')
+    {
+
+        buffer[Num]++;
+        ++Num;
+    }
+
+    printf("‘O‚Ì•¶Žš:%s\n", Old_buffer);
+    printf("‚¸‚ç‚µ‚½•¶Žš:%s\n", buffer);
+
+
+    /*for (int i = 0; i < k; ++i)
+    {
+        printf("%c\n", buffer[i]);
+    }*/
+
+    
+
+
+
+    system("PAUSE");
+
+    return 0;
 }
