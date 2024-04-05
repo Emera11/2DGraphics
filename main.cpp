5a0b6cc0d7a4b986a804d700f40311e9383336e7
Emera <emeraldedge_20230823@hotmail.com>
2024-04-06 00:52:15 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,45 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
+#define E 0.000001 
+
+double my_sqrt(double a)
+{
+    a = a < 0 ? -a : a;
+    double x = a / 2;
+    while (1) {
+        double e = x * x - a;
+        double t = e < 0 ? -e : e;
+        if (t < E) return x;
+        x -= e / (x * 2);
+    }
+}
 
 int main(void)
-{
-    char buffer[10]{'\0'};
-    char Old_buffer[10]{'\0'};
+{    
 
-    const int k = (sizeof(buffer) / sizeof(buffer[0]));
-    printf("•¶Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢...\n\n");
+    /*double input_value[5]{0.0};
 
- 
-     fgets(buffer, sizeof(buffer), stdin)
+    double common{0.0};
+    int i = 0;
 
-
-    int Num = 0;
-
-    memcpy(Old_buffer, buffer, sizeof(buffer));
-
-
-    
-
-    while (buffer[Num] != '\0')
+    for (i = 0; i < 5; ++i)
     {
+        cin >> input_value[i];
+    }
+    */
 
-        buffer[Num]++;
-        ++Num;
+   /* common = (input_value[0 + 1] / input_value[0]);
+    cout << endl;
+
+    for (i = 0; i < 4; ++i)
+    {
+        cout << common << endl;
+
+        if ((input_value[i+1] / input_value[i]) != common)
+        {
+            break;
+        }
     }
 
-    printf("‘O‚Ì•¶Žš:%s\n", Old_buffer);
-    printf("‚¸‚ç‚µ‚½•¶Žš:%s\n", buffer);
-
-
-    /*for (int i = 0; i < k; ++i)
+    if (i == 4)
     {
-        printf("%c\n", buffer[i]);
+        cout << "“™”ä”—ñ‚Å‚·" << endl;
+    }
+    else
+    {
+        cout << "“™”ä”—ñ‚Å‚Í‚ ‚è‚Ü‚¹‚ñ" << endl;
     }*/
 
+    /*for (i = 0; i < 3; i++)
+    {
+        if (input_value[i] / input_value[i + 1] != input_value[i + 1] / input_value[i + 2])
+        {
+            //cout << input_value[i] / input_value[i + 1] << endl;
+            Hantei = 0.0;
+        }
+        else
+        {
+            cout << input_value[i] / input_value[i + 1] << endl;
+        }
+    }
+
+    if (Hantei == 1.0) {
+        cout << "“™”ä”—ñ‚Å‚·" << endl;
+    }
+    else
+    {
+        cout << "“™”ä”—ñ‚¶‚á‚È‚¢‚Å‚·" << endl;
+    }*/
+
+    /*const double pai = 3.14159265;
+    double min = 0.0;
+
+    int ii{0};
+    int jj{0};
+
+    double dif;
+    double dif_min = 999.9;
+
+
+    for (double i = 100.0; i < 1000; i+=1.0)
+    {
+        for (double j = 100.0; j < 1000; j += 1.0)
+        {
+           
+            if ((i / j) >= min && (i / j) <= pai)
+            {
+                
+                ii = i;
+                jj = j;
+                min = (i / j);
+            }
+
+            
+
+
+        }
+    }*/
+
+
+    //cout << "ˆê”ÔƒpƒC‚É‹ß‚¢‚Ì‚Í" << ii << "/" << jj << "‚Ì" << min << endl;
+
+
+  
     
+    for (int i = 1; i < 100; ++i)
+    {
+        for (int j = 1; j < 100; ++j)
+        {
+            if (my_sqrt(((i * i) + (j * j))) < 100)
+            {
+                cout << i << "*" << j << endl;
+            }
+        }
+        cout << endl;
+     }
 
-
+/*
+int a, b, c;
+        for (a = 1; a < 100; a++)
+        {
+            for (b = a; b < 100; b++)
+            {
+                for (c = b; c < 100; c++)
+                {
+                    if ((a * a) + (b * b) == c * c)
+                    {
+                        cout << a << "*" << b << "=" << c << endl;
+                    }
+                    
+                }
+            }
+            cout << endl;
+        }
+*/
 
     system("PAUSE");
 
